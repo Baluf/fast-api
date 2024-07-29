eeaa9bc20c0c192abcc23ef7657a6e94c2a46ea5
baraluf <alufbar8@gmail.com>
2024-07-29 13:43:37 +0300

    support update function.

--- blog/main.py
+++ blog/main.py
@@ -33,6 +33,19 @@ def all(db: Session = Depends(get_db)):
     return blogs
 
 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, response: Response, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+
+    blog.update({'title': request.title, 'body': request.body})
+    db.commit()
+    return 'updated'
+
+
 @app.get('/blog/{id}', status_code=status.HTTP_200_OK)
 def show(id, response: Response, db: Session = Depends(get_db), status_code: int = status.HTTP_200_OK):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
@@ -46,6 +59,6 @@ def show(id, response: Response, db: Session = Depends(get_db), status_code: int
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     db.commit()
     return 'done'
