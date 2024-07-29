3928304f3a2827026492552843d836dc1366d2a2
baraluf <alufbar8@gmail.com>
2024-07-29 15:33:18 +0300

    add response_model to CRUD.

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI, Depends, Response, HTTPException
 from starlette import status
 
@@ -27,7 +29,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog',  response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
@@ -46,7 +48,7 @@ def update(id, response: Response, request: schemas.Blog, db: Session = Depends(
     return 'updated'
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db), status_code: int = status.HTTP_200_OK):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
 
@@ -59,6 +61,11 @@ def show(id, response: Response, db: Session = Depends(get_db), status_code: int
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
