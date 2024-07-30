90ba4032f22a2567925e86462baef915d4b070f6
baraluf <alufbar8@gmail.com>
2024-07-31 00:42:11 +0300

    move internal logic from routers to repository/blog.py

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -4,22 +4,24 @@ from typing import List
 from sqlalchemy.orm import Session
 from starlette import status
 
+from ..repository import blog
+
 router = APIRouter(prefix='/blog', tags=['blogs'])
 
 
 @router.get('/', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(database.get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return blog.get_all(db)
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(request, db)
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(database.get_db)):
+    return blog.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
@@ -44,15 +46,3 @@ def show(id, db: Session = Depends(database.get_db), status_code: int = status.H
                             detail={'detail': f'Blog with id does not exist {id}'})
 
     return blog
-
-
-@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(database.get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Blog with id {id} not found")
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return 'done'
