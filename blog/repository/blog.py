30f89ba9fd83b79cd97b7925d666031d8411d089
baraluf <alufbar8@gmail.com>
2024-07-31 16:51:39 +0300

    move show and update dunc to repository blog.py

--- blog/repository/blog.py
+++ blog/repository/blog.py
@@ -27,3 +27,25 @@ def destroy(id, db: Session):
     blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
+
+
+def update(id, request: schemas.Blog, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+
+    blog.update({'title': request.title, 'body': request.body})
+    db.commit()
+    return 'updated'
+
+
+def show(id, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={'detail': f'Blog with id does not exist {id}'})
+
+    return blog
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -26,23 +26,9 @@ def destroy(id, db: Session = Depends(database.get_db)):
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(database.get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Blog with id {id} not found")
-
-    blog.update({'title': request.title, 'body': request.body})
-    db.commit()
-    return 'updated'
+    return blog.update(id, request, db)
 
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def show(id, db: Session = Depends(database.get_db), status_code: int = status.HTTP_200_OK):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail={'detail': f'Blog with id does not exist {id}'})
-
-    return blog
+def show(id, db: Session = Depends(database.get_db)):
+    return blog.show(id, db)
