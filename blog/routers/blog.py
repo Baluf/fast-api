73d92ad8d92b9df133ee8d57f51f6ef364931b81
baraluf <alufbar8@gmail.com>
2024-08-02 23:26:47 +0300

    add current_user verification to all endpoints.

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -17,20 +17,22 @@ def all(db: Session = Depends(database.get_db), current_user: schemas.User = Dep
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
+def create(request: schemas.Blog, db: Session = Depends(database.get_db),
+           current_user: schemas.User = Depends(outh2.get_current_user)):
     return blog.create(request, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(database.get_db)):
+def destroy(id, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(outh2.get_current_user)):
     return blog.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(database.get_db)):
+def update(id, request: schemas.Blog, db: Session = Depends(database.get_db),
+           current_user: schemas.User = Depends(outh2.get_current_user)):
     return blog.update(id, request, db)
 
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def show(id, db: Session = Depends(database.get_db)):
+def show(id, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(outh2.get_current_user)):
     return blog.show(id, db)
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -3,12 +3,13 @@ from .. import schemas, database, models
 
 from sqlalchemy.orm import Session
 from ..hashing import Hash
-
+from . import outh2
 router = APIRouter(prefix='/user', tags=['Users'])
 
 
 @router.post('/', response_model=schemas.ShowUser)
-def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
+def create_user(request: schemas.User, db: Session = Depends(database.get_db),
+                current_user: schemas.User = Depends(outh2.get_current_user)):
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
@@ -17,7 +18,7 @@ def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
 
 
 @router.get('/{id}', response_model=schemas.ShowUser)
-def get_user(id, db: Session = Depends(database.get_db)):
+def get_user(id, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(outh2.get_current_user)):
     user = db.query(models.User).filter(models.User.id == id).first()
 
     if not user:
